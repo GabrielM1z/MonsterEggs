6337a01ce8c08823c75c42ca7c9a5c420842b560
IUT-Toulon-Maxence-Jamme <78029916+IUT-Toulon-Maxence-Jamme@users.noreply.github.com>
2024-01-30 10:02:43 +0100

    mise en place du test

--- DemoHelloWorld/src/main/java/com/example/demohelloworld/web/controller/TestController.java
+++ DemoHelloWorld/src/main/java/com/example/demohelloworld/web/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.example.demohelloworld.web.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TestController {
     @GetMapping("/helloTest")
     public String helloTest(){
