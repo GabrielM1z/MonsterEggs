f2ab93fd3647b870e83a896787d679d058ebc131
PascualDamien34 <damien.pascual34@gmail.com>
2024-02-01 12:11:40 +0100

    push damien firts

--- Joueur/src/main/java/com/example/joueur/controller/InventaireController.java
+++ Joueur/src/main/java/com/example/joueur/controller/InventaireController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-public class JoueurController {
+public class InventaireController {
     @GetMapping("/main")
     public String main(){
         return "Hello GAB LE JOUEUR !";
